b47b054a9160bc090cf01949cee797a4c5a8f310
yael <yael.atletl@gmail.com>
2020-03-19 01:42:03 -0600

    Add quodot plugin, viewports now work

--- Viewports.gd
+++ Viewports.gd
@@ -1,4 +1,8 @@
+tool
 extends PanelContainer
+
+var status = "released"
+var mpos = Vector2()
 
 onready var Top = $HorizontalDiv/VerticalDivTop/TopLeft/Top
 onready var Left = $HorizontalDiv/VerticalDivBottom/BottomLeft/Left
@@ -57,3 +61,63 @@ func _on_BackGround_resized():
 	yield(get_tree(),"idle_frame")
 	yield(get_tree(),"idle_frame")
 	_resize_viewport()
+
+
+func _process(delta):
+	mpos *= delta
+	if status == "dragging TopLeft":
+		Top.get_node("Camera").translate(Vector3((mpos).x,(mpos).y,0))
+	if status == "dragging TopRight":
+		Front.get_node("Camera").translate(Vector3((mpos).x,(mpos).y,0))
+	if status == "dragging BottomLeft":
+		Left.get_node("Camera").translate(Vector3(mpos.x,(mpos).y,0))
+
+
+func _input(ev):
+	
+	if (status!=null or status != "released") and ev is InputEventMouseMotion:
+		print(status)
+		mpos=ev.relative
+	if status == "released":
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	
+
+func _on_TopLeft_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			status = "dragging TopLeft"
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			status = "released"
+
+
+
+func _on_BottomLeft_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			status = "dragging BottomLeft"
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			status = "released"
+			
+
+
+func _on_TopRigth_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			status = "dragging TopRight"
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			status = "released"
+			
+
+
+func _on_BottomRight_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			status = "dragging BottomRight"
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			status = "released"
+			
